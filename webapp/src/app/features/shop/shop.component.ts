3738c6762872a27e9dd0d83f784f1656e6d3e201
neuromagus <neuromagus@mail.ru>
2024-08-19 00:10:17 +0300

    added search (simple) in shop component

--- webapp/src/app/features/shop/shop.component.ts
+++ webapp/src/app/features/shop/shop.component.ts
@@ -12,6 +12,7 @@ import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angula
 import { ShopParams } from '../../shared/models/shopParams';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { Pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-shop',
@@ -25,7 +26,8 @@ import { Pagination } from '../../shared/models/pagination';
         MatSelectionList,
         MatListOption,
         MatMenuTrigger,
-        MatPaginator
+        MatPaginator,
+        FormsModule
     ],
     templateUrl: './shop.component.html',
     styleUrl: './shop.component.scss'
@@ -58,6 +60,11 @@ export class ShopComponent implements OnInit {
             next: response => this.products = response,
             error: error => console.log(error),
         })
+    }
+
+    onSearchChange() {
+        this.shopParams.pageNumber = 1
+        this.getProducts()
     }
 
     handlePageEvent(event: PageEvent) {
