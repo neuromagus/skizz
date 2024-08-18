6f3a47bbd8a89ce32aba7ebbcd6c8eb676679473
neuromagus <neuromagus@mail.ru>
2024-08-18 03:37:03 +0300

    created filters dialog, added shop service and added product cards

---
+++ webapp/src/app/core/services/shop.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Product } from '../../shared/models/product';
+import { Pagination } from '../../shared/models/pagination';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ShopService {
+    baseUrl = "https://localhost:5051/api/"
+    private http = inject(HttpClient)
+
+    types: string[] = []
+    brands: string[] = []
+
+    getProducts(brands?: string[], types?: string[]) {
+        let params = new HttpParams()
+
+        if (brands && brands.length > 0) {
+            params = params.append("brands", brands.join(","))
+        }
+
+        if (types && types.length > 0) {
+            params = params.append("types", types.join(","))
+        }
+        
+        params = params.append("pageSize", 20)
+
+        return this.http.get<Pagination<Product>>(this.baseUrl + "products", {params})
+    }
+
+    getBrands() {
+        if (this.brands.length > 0) return
+        return this.http.get<string[]>(this.baseUrl + "products/brands").subscribe({
+            next: response => this.brands = response
+        })
+    }
+
+    getTypes() {
+        if (this.types.length > 0) return
+        return this.http.get<string[]>(this.baseUrl + "products/types").subscribe({
+            next: response => this.types = response
+        })
+    }
+}
