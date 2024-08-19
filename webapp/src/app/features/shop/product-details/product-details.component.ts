6aab36710387f2ad0a759ae258128b00bfd2d6ed
neuromagus <neuromagus@mail.ru>
2024-08-19 03:19:46 +0300

    added styles for shop cards, add getProduct in Service

--- webapp/src/app/features/shop/product-details/product-details.component.ts
+++ webapp/src/app/features/shop/product-details/product-details.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ShopService } from '../../../core/services/shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../shared/models/product';
 
 @Component({
-  selector: 'app-product-details',
-  standalone: true,
-  imports: [],
-  templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+    selector: 'app-product-details',
+    standalone: true,
+    imports: [],
+    templateUrl: './product-details.component.html',
+    styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit{
+    private shopService = inject(ShopService)
+    private activatedRoute = inject(ActivatedRoute)
 
+    product?: Product
+
+    ngOnInit(): void {
+        this.loadProduct()
+    }
+
+    loadProduct() {
+        const id = this.activatedRoute.snapshot.paramMap.get("id")
+        if (!id) return
+        this.shopService.getProduct(+id).subscribe({
+            next: product => this.product = product,
+            error: error => console.log(error)
+        })
+    }
 }
