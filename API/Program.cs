96b5d115f1316e35314e4e0d2b1f2c1db0983938
neuromagus <neuromagus@mail.ru>
2024-08-21 17:32:06 +0300

    enabled Redis in the backend (connection options etc)

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,13 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config => {
+    var connectionString = builder.Configuration.GetConnectionString("Redis") 
+        ?? throw new Exception("Cannot get Redis connection string");
+    var configuration = ConfigurationOptions.Parse(connectionString, true);
+
+    return ConnectionMultiplexer.Connect(configuration);
+});
 
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
