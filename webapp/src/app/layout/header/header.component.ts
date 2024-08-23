add34d42e025dc6b7f0acbc0b414cd39e6186817
neuromagus <neuromagus@mail.ru>
2024-08-24 02:01:06 +0300

    designed basket (TODO - need implement signals)

--- webapp/src/app/layout/header/header.component.ts
+++ webapp/src/app/layout/header/header.component.ts
@@ -5,6 +5,7 @@ import { MatIcon } from '@angular/material/icon'
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
+import { CartService } from '../../core/services/cart.service';
 
 @Component({
     selector: 'app-header',
@@ -15,11 +16,12 @@ import { MatProgressBar } from '@angular/material/progress-bar';
         MatBadge,
         RouterLink,
         RouterLinkActive,
-        MatProgressBar
+        MatProgressBar        
     ],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
     busyService = inject(BusyService)
+    cartService = inject(CartService)
 }
