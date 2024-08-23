d99df8c329f8a31b8ace298c2048d1165e362a16
neuromagus <neuromagus@mail.ru>
2024-08-23 23:32:15 +0300

    added environment to the project and cart service

---
+++ webapp/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    // need change to the something.techdev.pro
+    apiUrl: "https://localhost:5051/api/"
+};
---
+++ webapp/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    // need change to the something.techdev.pro
+    apiUrl: "api/"
+};
+
+
