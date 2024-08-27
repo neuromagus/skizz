58fff9ecb095f2101cc18887aa6c264d62958dad
neuromagus <neuromagus@mail.ru>
2024-08-28 00:42:35 +0300

    added new entity (deliveryMethod) and created migration

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -12,11 +12,24 @@ public class StoreContextSeed
             var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
             var products = JsonSerializer.Deserialize<List<Product>>(productsData);
 
-            if(products is null) return;
+            if (products is null) return;
 
             context.Products.AddRange(products);
 
             await context.SaveChangesAsync();
         }
+
+        if (!context.DeliveryMethods.Any())
+        {
+            var dmData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/delivery.json");
+            var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
+
+            if (methods is null) return;
+
+            context.DeliveryMethods.AddRange(methods);
+
+            await context.SaveChangesAsync();
+        }
+
     }
 }
