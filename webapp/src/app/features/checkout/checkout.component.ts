aa9e633b98504904c84d7f9480ff299af29d3fda
neuromagus <neuromagus@mail.ru>
2024-08-28 02:58:12 +0300

    change style and set skeleton for material stepper in checkout component (looks great now ;))

--- webapp/src/app/features/checkout/checkout.component.ts
+++ webapp/src/app/features/checkout/checkout.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
+import { MatStepperModule } from "@angular/material/stepper";
 
 @Component({
-  selector: 'app-checkout',
-  standalone: true,
-  imports: [],
-  templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.scss'
+    selector: 'app-checkout',
+    standalone: true,
+    imports: [
+        OrderSummaryComponent,
+        MatStepperModule
+    ],
+    templateUrl: './checkout.component.html',
+    styleUrl: './checkout.component.scss'
 })
 export class CheckoutComponent {
 
