bed8dc20e742057875c248c5c4c02f50c3cc2685
neuromagus <neuromagus@mail.ru>
2024-08-29 04:11:28 +0300

    done with Stripe (confirmation, validations, tokens etc), create a skeleton of checkout success component

--- webapp/src/app/core/services/stripe.service.ts
+++ webapp/src/app/core/services/stripe.service.ts
@@ -1,5 +1,10 @@
 import { inject, Injectable } from '@angular/core';
-import { loadStripe, Stripe, StripeAddressElement, StripeAddressElementOptions, StripeElements, StripePaymentElement } from "@stripe/stripe-js"
+import {
+    ConfirmationToken,
+    loadStripe, Stripe,
+    StripeAddressElement, StripeAddressElementOptions,
+    StripeElements, StripePaymentElement
+} from "@stripe/stripe-js"
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CartService } from './cart.service';
@@ -18,7 +23,7 @@ export class StripeService {
     private elements?: StripeElements
     private addressElement?: StripeAddressElement
     private accountService = inject(AccountService)
-    private paymentElement?: StripePaymentElement 
+    private paymentElement?: StripePaymentElement
 
     constructor() {
         this.stripePromise = loadStripe(environment.stripePublicKey)
@@ -55,7 +60,6 @@ export class StripeService {
 
         return this.paymentElement
     }
-
 
     async createAddressElement() {
         if (!this.addressElement) {
@@ -102,6 +106,42 @@ export class StripeService {
                 return cart
             })
         )
+    }
+
+    async createConfirmationToken() {
+        const stripe = await this.getStripeInstance()
+        const elements = await this.initializeElements()
+        const result = await elements.submit()
+
+        if (result.error) throw new Error(result.error.message)
+
+        if (stripe) {
+            return await stripe.createConfirmationToken({ elements })
+        } else {
+            throw new Error("Stripe not available")
+        }
+    }
+
+    async confirmPayment(confirmationToken: ConfirmationToken) {
+        const stripe = await this.getStripeInstance()
+        const elements = await this.initializeElements()
+        const result = await elements.submit()
+
+        if (result.error) throw new Error(result.error.message)
+
+        const clientSecret = this.cartService.cart()?.clientSecret
+
+        if (stripe && clientSecret) {
+            return await stripe.confirmPayment({
+                clientSecret: clientSecret,
+                confirmParams: {
+                    confirmation_token: confirmationToken.id
+                },
+                redirect: "if_required"
+            })
+        } else {
+            throw new Error("Unable to load Stripe")
+        }
     }
 
     disposeElements() {
--- webapp/src/app/features/checkout/checkout.component.ts
+++ webapp/src/app/features/checkout/checkout.component.ts
@@ -1,10 +1,15 @@
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
-import { MatStepperModule } from "@angular/material/stepper";
-import { RouterLink } from '@angular/router';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
+import { MatStepper, MatStepperModule } from "@angular/material/stepper";
+import { Router, RouterLink } from '@angular/router';
 import { MatButton } from '@angular/material/button';
 import { StripeService } from '../../core/services/stripe.service';
-import { StripeAddressElement, StripeAddressElementChangeEvent, StripePaymentElement, StripePaymentElementChangeEvent } from '@stripe/stripe-js';
+import {
+    ConfirmationToken, StripeAddressElement,
+    StripeAddressElementChangeEvent,
+    StripePaymentElement, StripePaymentElementChangeEvent
+} from '@stripe/stripe-js';
 import { SnackbarService } from '../../core/services/snackbar.service';
 import { MatCheckboxChange, MatCheckboxModule } from "@angular/material/checkbox";
 import { StepperSelectionEvent } from '@angular/cdk/stepper';
@@ -28,7 +33,8 @@ import { CurrencyPipe, JsonPipe } from '@angular/common';
         CheckoutDeliveryComponent,
         CheckoutReviewComponent,
         CurrencyPipe,
-        JsonPipe
+        JsonPipe,
+        MatProgressSpinnerModule
     ],
     templateUrl: './checkout.component.html',
     styleUrl: './checkout.component.scss'
@@ -37,6 +43,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
     private stripeService = inject(StripeService)
     private snackBar = inject(SnackbarService)
     private accountService = inject(AccountService)
+    private router = inject(Router)
     cartService = inject(CartService)
     addressElement?: StripeAddressElement
     paymentElement?: StripePaymentElement
@@ -44,6 +51,8 @@ export class CheckoutComponent implements OnInit, OnDestroy {
     completionStatus = signal<{ address: boolean, card: boolean, delivery: boolean }>({
         address: false, card: false, delivery: false
     })
+    confirmationToken?: ConfirmationToken
+    loading = false
 
     async ngOnInit() {
         try {
@@ -78,7 +87,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
     handleDeliveryChange(event: boolean) {
         this.completionStatus.update(state => {
             state.delivery = event
-            
+
             return state
         })
     }
@@ -102,6 +111,33 @@ export class CheckoutComponent implements OnInit, OnDestroy {
         if (event.selectedIndex === 2) {
             await firstValueFrom(this.stripeService.createOrUpdatePaymentIntent())
         }
+
+        if (event.selectedIndex === 3) {
+            await this.getConfirmationToken()
+        }
+    }
+
+    async confirmPayment(stepper: MatStepper) {
+        this.loading = true
+
+        try {
+            if (this.confirmationToken) {
+                const result = await this.stripeService.confirmPayment(this.confirmationToken)
+                
+                if (result.error) {
+                    throw new Error(result.error.message)
+                } else {
+                    this.cartService.deleteCart()
+                    this.cartService.selectedDelivery.set(null)
+                    this.router.navigateByUrl("/checkout/success")
+                }
+            }
+        } catch (error: any) {
+            this.snackBar.error(error.message || "Something went wrong")
+            stepper.previous()
+        } finally {
+            this.loading = false
+        }        
     }
 
     private async getAddressFromStripeAddress(): Promise<Address | null> {
@@ -118,5 +154,17 @@ export class CheckoutComponent implements OnInit, OnDestroy {
                 postalCode: address.postal_code
             }
         } else return null
+    }
+
+    async getConfirmationToken() {
+        try {
+            if (Object.values(this.completionStatus()).every(status => status === true)) {
+                const result = await this.stripeService.createConfirmationToken()
+                if (result.error) throw new Error(result.error.message)
+                this.confirmationToken = result.confirmationToken
+            }
+        } catch (error: any) {
+            this.snackBar.error(error.message)
+        }
     }
 }
