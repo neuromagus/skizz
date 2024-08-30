797050947d67c8b166cdfee24ee9507d6ccada36
neuromagus <neuromagus@mail.ru>
2024-08-30 04:08:58 +0300

    Implementation UnitOfWork (reflection, dude)

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddCors();
 builder.Services.AddSingleton<IConnectionMultiplexer>(config => {
     var connectionString = builder.Configuration.GetConnectionString("Redis") 
