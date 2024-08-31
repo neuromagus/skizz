375bd1d35fb5d09c27b392e21c0f01d6d2adf40d
neuromagus <neuromagus@mail.ru>
2024-08-31 05:50:13 +0300

    fix one field in PaymentSummary

--- Core/Entities/OrderAggregate/PaymentSummary.cs
+++ Core/Entities/OrderAggregate/PaymentSummary.cs
@@ -5,6 +5,6 @@ public class PaymentSummary
     public int Last4 { get; set; }
     public required string Brand { get; set; }
     public int ExpMonth { get; set; }
-    public int Year { get; set; }
+    public int ExpYear { get; set; }
 
 }
