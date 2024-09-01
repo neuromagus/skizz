b1ff81cd34dddfe73c202d90f3bab313dd762c2e
neuromagus <neuromagus@mail.ru>
2024-09-01 08:23:58 +0300

    remove images and add one ;} set home component for simple way

--- webapp/src/app/features/home/home.component.ts
+++ webapp/src/app/features/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    imports: [
+        MatButton,
+        RouterLink
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
